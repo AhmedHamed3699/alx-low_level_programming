92ff6148d21c7168e112c2a3d467702165c94e4a
Ahmed Hamed <Ahmed.Hamed03@eng-st.cu.edu.eg>
2023-07-14 22:07:22 +0000

    Ended last advanced task

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * program that prints
+ * all possible different combinations of two two-digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			for (k = i; k <= '9'; k++)
+			{
+				if (k == i)
+					l = j + 1;
+				else
+					l = '0';
+
+				for (; l <= '9'; l++)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(k);
+					putchar(l);
+					if (i != '9' || j != '8' || k != '9' || l != '9')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
