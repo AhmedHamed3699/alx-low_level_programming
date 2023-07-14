73dd3e131f273ca384381007d8a917104bac2d99
Ahmed Hamed <Ahmed.Hamed03@eng-st.cu.edu.eg>
2023-07-14 19:31:49 +0000

    Edited all tasks for proper compilation

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -14,6 +14,6 @@ int main(void)
 	for (c = '0'; c <= '9'; c++)
 		putchar(c);
 
-	putchar("\n");
+	putchar('\n');
 	return (0);
 }
--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -15,6 +15,6 @@ int main(void)
 	for (c = 'z'; c >= 'a'; c--)
 		putchar(c);
 
-	putchar("\n");
+	putchar('\n');
 	return (0);
 }
