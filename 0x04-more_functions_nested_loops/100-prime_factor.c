1f50bf73f17f8aabf3421b2429b924d38f04a63e
Ahmed Hamed <Ahmed.Hamed03@eng-st.cu.edu.eg>
2023-07-20 19:13:47 +0000

    Ended advanced tasks

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - code entry
+ * get largest prime factor of a number
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	unsigned long int n, i, ans;
+
+	n = 612852475143;
+	i = 2;
+	ans = 1;
+	while (i * i <= n)
+	{
+		while (n % i == 0)
+		{
+			n /= i;
+			ans = i;
+		}
+		i++;
+	}
+	printf("%ld\n", ans);
+	return (0);
+}
