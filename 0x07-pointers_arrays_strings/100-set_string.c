bd02a73e8cf3f68124dbf85ee20639ae438238dc
Ahmed Hamed <Ahmed.Hamed03@eng-st.cu.edu.eg>
2023-07-31 20:38:14 +0000

    done with task 9

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * set_string - sets the value of a pointer to a char
+ * of a square matrix of integers
+ * @s: main string
+ * @to: string to be setted
+ *
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
