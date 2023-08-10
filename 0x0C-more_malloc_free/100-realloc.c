b3cc4347321e7886b9ff90fe89bb2c54ef952e6e
Ahmed Hamed <Ahmed.Hamed03@eng-st.cu.edu.eg>
2023-08-11 02:17:58 +0300

    Edited task 5

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -18,6 +18,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_size == old_size)
 		return (ptr);
+
 	if (new_size == 0)
 	{
 		free(ptr);
@@ -26,7 +27,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	newPtr = malloc(new_size);
 	if (newPtr == NULL)
-		return (NULL);
+		return (ptr);
 
 	if (ptr == NULL)
 		return (newPtr);
