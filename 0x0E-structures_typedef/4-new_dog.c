0eb6f238d5551e61757f2beb5058a67598f98396
Ahmed Hamed <Ahmed.Hamed03@eng-st.cu.edu.eg>
2023-08-15 23:29:36 +0300

    Edited task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -19,7 +19,18 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (NewDog == NULL)
 		return (NULL);
 	NewDog->name = strdup(name);
+	if (NewDog->name == NULL && (name != NULL || name[0] != '\0'))
+	{
+		free(NewDog);
+		return (NULL);
+	}
 	NewDog->owner = strdup(owner);
+	if (NewDog->owner == NULL && (name != NULL || name[0] != '\0'))
+	{
+		free(NewDog->name);
+		free(NewDog);
+		return (NULL);
+	}
 	NewDog->age = age;
 	return (NewDog);
 }
